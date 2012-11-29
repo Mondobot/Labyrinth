4f45c811c3c526a60f0530d4e2ffa272d9e11dbc
Matei Pavaluca <mateipavaluca@yahoo.com>
2012-11-29 20:38:47 +0200

    done conceptual main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
+/*
+ * Includes go here:
+ */
+#include <stdlib.h>
+#include <iostream>
+
 
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		std::cout << "Unrecognised number of arguments!\n";
-		std::cout << "(got " << argc << "%d, expected 1)\n"
+		std::cout << "(got " << argc << "%d, expected 1)\n";
 		exit(1);
 	}
 
-	MazeGame game = new MazeGame(argv[1])
+	MazeGame game = new MazeGame();
+
+	game.init();
+	game.run();
+
+	return 0;
+}
